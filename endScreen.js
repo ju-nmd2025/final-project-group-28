f4884da82b91fb87c579e1cef6a0bc46262c5529
Lisa <lisa.vikman@telia.com>
2025-12-15 22:16:41 +0100

    changed end screen
    changed the code for end screen

--- endScreen.js
+++ endScreen.js
@@ -1,20 +1,56 @@
 export class EndScreen {
+  constructor(onRetry, options = {}) {
+    this.onRetry = onRetry;
 
-function setup() {
-  createCanvas(400, 400);
+    // retry button geometry (centered)
+    this.buttonX = 85;
+    this.buttonY = 150;
+    this.buttonW = 230;
+    this.buttonH = 70;
 
-  background(255, 150, 250);
+    this.title = options.title ?? "Game Over";
+  }
 
-  strokeWeight(6);
+  draw() {
+    push();
+    background(255, 150, 250);
 
-  rect(70, 80, 250, 250);
-}
+    // Title
+    textAlign(CENTER, CENTER);
+    noStroke();
+    fill(255);
+    textSize(44);
+    text(this.title, width / 2, height * 0.25);
 
-function draw() {
-  textSize(20);
-  text("you died", 160, 170);
+    // Draw retry button
+    stroke(0);
+    strokeWeight(2);
+    fill(255, 255, 255);
+    rect(this.buttonX, this.buttonY, this.buttonW, this.buttonH, 40);
 
-  rect(96, 225, 200, 70);
-  text("restart game", 140, 268);
-}
+    // Label
+    noStroke();
+    fill(0);
+    textSize(18);
+    text(
+      "Retry",
+      this.buttonX + this.buttonW / 2,
+      this.buttonY + this.buttonH / 2
+    );
+
+    pop();
+  }
+
+  mousePressed(mx, my) {
+    if (
+      mx > this.buttonX &&
+      mx < this.buttonX + this.buttonW &&
+      my > this.buttonY &&
+      my < this.buttonY + this.buttonH
+    ) {
+      if (typeof this.onRetry === "function") this.onRetry();
+      return true;
+    }
+    return false;
+  }
 }
